a4025fcceff5dc0f6a9bbf6e984878bd12925e7d
Ryota-Nakamura-317 <nkmr94317rt@gmail.com>
2021-01-01 02:12:54 +0900

    isupdateで条件分岐 addで加えるか、updateで上書きするか

--- lib/presentation/add_book/add_book_model.dart
+++ lib/presentation/add_book/add_book_model.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test1/domain/book.dart';
 
 class AddBookModel extends ChangeNotifier {
   String bookTitle = '';
+
   Future addBookToFirebase() async {
     if (bookTitle.isEmpty) {
       throw ('タイトルを入力してください');
@@ -11,6 +13,17 @@ class AddBookModel extends ChangeNotifier {
       {
         'title': bookTitle,
         'createdAt': Timestamp.now(),
+      },
+    );
+  }
+
+  Future updateBook(Book book) async {
+    final document =
+        FirebaseFirestore.instance.collection('books').doc(book.documentID);
+    await document.update(
+      {
+        'title': bookTitle,
+        'updateAt': Timestamp.now(),
       },
     );
   }
