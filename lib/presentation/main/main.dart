c64d217966c97cecdd4bfd4b4bc9fbfa9be8eb0e
Ryota-Nakamura-317 <nkmr94317rt@gmail.com>
2021-01-01 22:43:05 +0900

    新規登録（email) ログイン

--- lib/presentation/main/main.dart
+++ lib/presentation/main/main.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test1/presentation/book_list/book_list_page.dart';
+import 'package:flutter_test1/presentation/login/login_page.dart';
 import 'package:flutter_test1/presentation/main/main_model.dart';
+import 'package:flutter_test1/presentation/signup/signup_page.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -32,12 +33,22 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                   RaisedButton(
-                    child: Text('ボタン'),
+                    child: Text('新規登録'),
                     onPressed: () {
                       //ここで何か
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => BookListPage()),
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('ログイン'),
+                    onPressed: () {
+                      //ここで何か
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
                       );
                     },
                   ),
